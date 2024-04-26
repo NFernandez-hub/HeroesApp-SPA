6231d67d73d82515036dfdf08aa75b67f698bb2e
Nicolas Fernandez <nfernandez.dev@gmail.com>
2024-04-25 21:11:13 -0300

    Primer Commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+import { HeroeList } from "../components"
+
+export const DcPage = () => {
+    return (
+        <>
+            <h1>Dc Page</h1>
+            <hr />
+
+            <HeroeList publisher='DC Comics' />
+        </>
+    )
+}
